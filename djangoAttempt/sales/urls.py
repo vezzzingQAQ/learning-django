ea617a4ccc853dfb4a099b11472f5286bf97635f
vezzzingdesktop <1932966162@qq.com>
2021-08-02 14:59:23 +0800

    gui

--- djangoAttempt/sales/urls.py
+++ djangoAttempt/sales/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('orders/', views.listorders),
     #修改路由表， 加上对 sales/customers/ url请求的 路由
     path('customers/',views.listcustomers),
+    path('customersGui/',views.listcustomersWithGUI1),
 ]
--- djangoAttempt/sales/views.py
+++ djangoAttempt/sales/views.py
@@ -1,9 +1,54 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
 #导入数据库
 from common.models import Customer
+#导入引擎
+from django.template import engines
+#*****************************************************************
+# 先定义好HTML模板
+html_template ='''
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+table {
+    border-collapse: collapse;
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+</style>
+</head>
+    <body>
+        <table>
+        <tr>
+        <th>id</th>
+        <th>姓名</th>
+        <th>电话号码</th>
+        <th>地址</th>
+        </tr>
+
+        {% for customer in customers %}
+            <tr>
+
+            {% for name, value in customer.items %}            
+                <td>{{ value }}</td>            
+            {% endfor %}
+            
+            </tr>
+        {% endfor %}
+                
+        </table>
+    </body>
+</html>
+'''
+
+django_engine = engines['django']
+template = django_engine.from_string(html_template)
 #凡是浏览器访问的http 请求的 url 地址 是 /sales/orders/ , 
 #就由 views.py 里面的函数 listorders 来处理， 返回一段字符串给浏览器
 # Create your views here.
@@ -29,3 +74,19 @@ def listcustomers(request):
         retStr += '<br>'
 
     return HttpResponse(retStr)
+
+def listcustomersWithGUI1(request):
+    # 返回一个 QuerySet 对象 ，包含所有的表记录
+    qs = Customer.objects.values()
+
+    # 检查url中是否有参数phonenumber
+    ph =  request.GET.get('phonenumber',None)
+
+    # 如果有，添加过滤条件
+    if ph:
+        qs = qs.filter(phonenumber=ph)
+
+    # 传入渲染模板需要的参数
+    rendered = template.render({'customers':qs})
+
+    return HttpResponse(rendered)
