2ec965240bf5ae5a5dd8320e941b5bced63ebdb6
vezzzingdesktop <1932966162@qq.com>
2021-08-06 19:47:46 +0800

    log in out

---
+++ djangoAttempt/mgr/sign_in_out.py
@@ -0,0 +1,39 @@
+#处理登录登出
+#2021.8.6
+from django.http import JsonResponse
+
+from django.contrib.auth import authenticate, login, logout
+
+# 登录处理
+def signin( request):
+    # 从 HTTP POST 请求中获取用户名、密码参数
+    userName = request.POST.get('username')
+    passWord = request.POST.get('password')
+
+    # 使用 Django auth 库里面的 方法校验用户名、密码
+    user = authenticate(username=userName, password=passWord)
+    
+    # 如果能找到用户，并且密码正确
+    if user is not None:
+        if user.is_active:#有这个字段，判断用户是否被禁用了
+            if user.is_superuser:
+                login(request, user)
+                # 在session中存入用户类型
+                request.session['usertype'] = 'mgr'
+
+                return JsonResponse({'ret': 0})
+            else:
+                return JsonResponse({'ret': 1, 'msg': '请使用管理员账户登录'})
+        else:
+            return JsonResponse({'ret': 0, 'msg': '用户已经被禁用'})
+        
+    # 否则就是用户名、密码有误
+    else:
+        return JsonResponse({'ret': 1, 'msg': '用户名或者密码错误'})
+
+
+# 登出处理
+def signout( request):
+    # 使用登出方法
+    logout(request)
+    return JsonResponse({'ret': 0})
--- djangoAttempt/mgr/urls.py
+++ djangoAttempt/mgr/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path
 
 from mgr import customer
+from mgr import sign_in_out
 
 urlpatterns = [
     path('customers',customer.dispatcher),
+    path('signin',sign_in_out.signin),
+    path('signout',sign_in_out.signout),
 ]
