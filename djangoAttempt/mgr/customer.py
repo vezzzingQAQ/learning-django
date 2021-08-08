3641ade4c156769f9bffb18ae78bd1a7fbf2fde2
vezzzingdesktop <1932966162@qq.com>
2021-08-08 12:39:54 +0800

    login cookey

--- djangoAttempt/mgr/customer.py
+++ djangoAttempt/mgr/customer.py
@@ -5,6 +5,22 @@ import json
 from common.models import Customer
 
 def dispatcher(request):
+    #api/mgr/....
+    # 根据session判断用户是否是登录的管理员用户
+    if 'usertype' not in request.session:
+        return JsonResponse({
+            'ret': 302,
+            'msg': '未登录',
+            'redirect': '/mgr/sign.html'}, #重定向
+            status=302)
+
+    if request.session['usertype'] != 'mgr' :
+        return JsonResponse({
+            'ret': 302,
+            'msg': '用户非mgr类型',
+            'redirect': '/mgr/sign.html'} ,
+            status=302)
+
     # 将请求参数统一放入request 的 params 属性中，方便后续处理
 
     # GET请求 参数 在 request 对象的 GET属性中
